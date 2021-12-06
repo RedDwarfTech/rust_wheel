42b240324cd5d550391d38136abe9cf38ebe305d
jiang qiang <jiangtingqiang@gmail.com>
2021-12-06 14:11:59 +0800

    [feature] tweak

---
+++ src/common/util/time_util.rs
@@ -0,0 +1,16 @@
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
+
+pub fn get_current_millisecond() -> i64{
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let in_ms = since_the_epoch.as_secs() * 1000 +
+        since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+    return in_ms as i64;
+}
+
+
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
 pub mod model;
+pub mod common;
 
 #[cfg(test)]
 mod tests {
+    use crate::common::util::time_util::get_current_millisecond;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn get_milliseconds(){
+        let mini = get_current_millisecond();
+        print!("{}",mini)
     }
 }
