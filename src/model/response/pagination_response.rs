68b9de8fd265e04757d950773578e41ca0b1e8e2
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-12-19 02:41:36 -0500

    [feature] tweak

--- src/model/response/pagination_response.rs
+++ src/model/response/pagination_response.rs
@@ -2,7 +2,7 @@ use crate::model::response::pagination::Pagination;
 use rocket::serde::Deserialize;
 use rocket::serde::Serialize;
 
-#[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Eq, Deserialize, Serialize,Default)]
 #[allow(non_snake_case)]
 pub struct PaginationResponse<T> {
     pub pagination: Pagination,
