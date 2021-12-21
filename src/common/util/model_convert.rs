9763011fa659d86d326f6353448eb84e43d939fd
jiang qiang <jiangtingqiang@gmail.com>
2021-12-21 13:26:31 +0800

    [feature] tweak model convert parameter type

--- src/common/util/model_convert.rs
+++ src/common/util/model_convert.rs
@@ -3,11 +3,11 @@ use crate::model::response::pagination::Pagination;
 use crate::model::response::pagination_response::PaginationResponse;
 
 
-pub fn map_pagination_res<U>(result: QueryResult<(Vec<U>, i64)>, page_num: i64,page_size: i64) -> PaginationResponse<Vec<U>>{
+pub fn map_pagination_res<U>(result: QueryResult<(Vec<U>, i64, i64)>, page_num: i64,page_size: i64) -> PaginationResponse<Vec<U>>{
     let page_result = Pagination{
         pageNum: page_num,
         pageSize: page_size,
-        total: 20
+        total: result.unwrap().2
     };
     let resp = PaginationResponse{
         pagination: page_result,
