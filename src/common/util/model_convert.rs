b4b2e4c1eaa218f326703c483ef23ec5714ca3e8
jiang qiang <jiangtingqiang@gmail.com>
2021-12-24 12:54:29 +0800

    [feature] add query result box function

--- src/common/util/model_convert.rs
+++ src/common/util/model_convert.rs
@@ -1,4 +1,8 @@
 use diesel::QueryResult;
+use rocket::response::content;
+use rocket::serde::json::serde_json;
+use serde::Serialize;
+use crate::model::response::api_response::ApiResponse;
 use crate::model::response::pagination::Pagination;
 use crate::model::response::pagination_response::PaginationResponse;
 
@@ -13,4 +17,13 @@ pub fn map_pagination_res<U>(result: QueryResult<(Vec<U>, i64, i64)>, page_num: 
         list: result.unwrap().0
     };
     return resp;
+}
+
+pub fn box_rest_response<T>(data: T) -> content::Json<String> where T: Serialize + Default {
+    let res = ApiResponse {
+        result: data,
+        ..Default::default()
+    };
+    let response_json = serde_json::to_string(&res).unwrap();
+    return content::Json(response_json);
 }
