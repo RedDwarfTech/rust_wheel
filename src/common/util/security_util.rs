8521dd179bad5a890ed79fedd446d0baf429d3fd
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-12-26 04:13:50 -0500

    [feature] tweak

--- src/common/util/security_util.rs
+++ src/common/util/security_util.rs
@@ -2,7 +2,7 @@ use ring::digest;
 use data_encoding::HEXLOWER;
 
 pub fn get_sha(password: String, salt: &str) -> String {
-    let calc = password.to_string() + salt;
+    let calc = salt.to_owned() + &password.to_string();
     let actual = digest::digest(&digest::SHA512, calc.as_ref());
     let calc_digest_str = HEXLOWER.encode(actual.as_ref());
     return calc_digest_str;
