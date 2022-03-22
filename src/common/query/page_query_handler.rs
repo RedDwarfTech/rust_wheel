f84b8489c865d7eb2304feacc93e90b9bdb8a611
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-03-23 02:42:08 +0800

    [fix] compile error

--- src/common/query/page_query_handler.rs
+++ src/common/query/page_query_handler.rs
@@ -1,9 +1,10 @@
 use diesel::pg::Pg;
 use diesel::query_builder::{AstPass, QueryFragment};
+use diesel::QueryResult;
 use diesel::sql_types::BigInt;
-use crate::pagination::Paginated;
+use crate::common::query::pagination::Paginated;
 
-pub fn handle_table_query<T: QueryFragment<Pg>>(this: &Paginated<T>, mut out: AstPass<Pg>){
+pub fn handle_table_query<T: QueryFragment<Pg>>(this: &Paginated<T>, mut out: AstPass<Pg>) -> QueryResult<()> {
     // https://stackoverflow.com/questions/6218902/the-sql-over-clause-when-and-why-is-it-useful
     out.push_sql("SELECT *, COUNT(*) OVER () FROM ");
     if this.is_sub_query {
@@ -18,9 +19,10 @@ pub fn handle_table_query<T: QueryFragment<Pg>>(this: &Paginated<T>, mut out: As
     out.push_sql(" OFFSET ");
     let offset = (this.page - 1) * this.per_page;
     out.push_bind_param::<BigInt, _>(&offset)?;
+    Ok(())
 }
 
-pub fn handle_big_table_query<T: QueryFragment<Pg>>(this: &Paginated<T>, mut out: AstPass<Pg>){
+pub fn handle_big_table_query<T: QueryFragment<Pg>>(this: &Paginated<T>, mut out: AstPass<Pg>)-> QueryResult<()>{
     out.push_sql("SELECT *, count_estimate('select * from article') FROM ");
     if this.is_sub_query {
         out.push_sql("(");
@@ -34,5 +36,6 @@ pub fn handle_big_table_query<T: QueryFragment<Pg>>(this: &Paginated<T>, mut out
         out.push_bind_param::<BigInt, _>(&offset)?;
         out.push_sql(")");
     }
+    Ok(())
 }
 
