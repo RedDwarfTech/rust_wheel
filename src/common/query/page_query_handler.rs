3bb661ab0828774ceaeb34661f0c89390323c739
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-03-23 19:09:13 +0800

    [feature] add dynamic table name

--- src/common/query/page_query_handler.rs
+++ src/common/query/page_query_handler.rs
@@ -24,8 +24,8 @@ pub fn handle_table_query<T: QueryFragment<Pg>>(this: &Paginated<T>, mut out: As
 }
 
 pub fn handle_big_table_query<T: QueryFragment<Pg>>(this: &PgBigTablePaginated<T>, mut out: AstPass<Pg>)-> QueryResult<()>{
-    // let select_str = format!("SELECT *, count_estimate('select * from {}') FROM  ", tablename);
-    out.push_sql("SELECT *, count_estimate('select * from article') FROM ");
+    let select_str = format!("SELECT *, count_estimate('select * from {}') FROM  ", this.table_name);
+    out.push_sql(select_str.as_str());
     if this.is_sub_query {
         out.push_sql("(");
     }
