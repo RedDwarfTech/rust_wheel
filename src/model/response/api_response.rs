c82f784774de35e8b489d89bd5b7197e62b6d8ff
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-06-13 21:07:59 +0800

    feat: tweak

--- src/model/response/api_response.rs
+++ src/model/response/api_response.rs
@@ -3,8 +3,10 @@ use rocket::response::Responder;
 use rocket::serde::Deserialize;
 use rocket::serde::Serialize;
 use rocket::{Request, Response};
+use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::okapi::schemars;
 
-#[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Eq, Deserialize, JsonSchema, Serialize)]
 #[allow(non_snake_case)]
 pub struct ApiResponse<T> {
     pub result: T,
