8e47f6e11b9894984adc1e7d96d817444c3e0064
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-30 15:45:46 +0800

    refactor: borrow the response when check success

--- src/common/util/response_handler.rs
+++ src/common/util/response_handler.rs
@@ -1,6 +1,6 @@
 use crate::model::response::api_response::ApiResponse;
 
-pub fn success<T>(resp: ApiResponse<T>) -> bool {
+pub fn success<T>(resp: &ApiResponse<T>) -> bool {
     if resp.resultCode == "200" && resp.statusCode == "200" {
         return true;
     }
