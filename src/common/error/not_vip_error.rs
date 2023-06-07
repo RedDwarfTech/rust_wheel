940624e5b314b32b4c01c6afab6219029c22c595
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-07 14:18:29 +0800

    feat: add vip expired error

---
+++ src/common/error/not_vip_error.rs
@@ -0,0 +1,14 @@
+
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+struct NotVipError;
+
+impl fmt::Display for NotVipError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "vip-expired")
+    }
+}
+
+impl Error for NotVipError {}
--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,2 +1,3 @@
 pub mod util;
 pub mod query;
+pub mod error;
