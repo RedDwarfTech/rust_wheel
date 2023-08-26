af7ca7e52f3857540ba3f8be4700437318731cbc
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-26 17:23:37 +0800

    feat: add file public function

--- src/common/util/rd_file_util.rs
+++ src/common/util/rd_file_util.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use log::error;
+use std::path::Path;
 
 pub fn create_folder_not_exists(folder: &String){
     match fs::create_dir(&folder) {
@@ -12,4 +13,14 @@ pub fn create_folder_not_exists(folder: &String){
             }
         }
     }
+}
+
+pub fn get_filename_without_ext(file_path: &String) -> &str{
+    let path = Path::new(file_path);
+    if let Some(file_name) = path.file_stem() {
+        if let Some(file_name_str) = file_name.to_str() {
+            return file_name_str;
+        }
+    }
+    return "unkown";
 }
