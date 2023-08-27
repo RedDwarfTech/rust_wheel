e2e33b3dbbd478a08000408e2305a0ce9ddcf96b
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-27 21:42:06 +0800

    refactor: tweak

--- src/common/wrapper/actix_http_resp.rs
+++ src/common/wrapper/actix_http_resp.rs
@@ -1,10 +1,13 @@
-use actix_web::{Responder, HttpResponse};
-use serde::Serialize;
 use crate::model::response::api_response::ApiResponse;
+use actix_web::HttpResponse;
+use serde::Serialize;
 
 /// sometimes we need to keep the response type for docs purpose
 /// https://github.com/GREsau/okapi/issues/102
-pub fn box_type_rest_response<T>(data: T) -> ApiResponse<T> where T: Serialize + Default {
+pub fn box_type_rest_response<T>(data: T) -> ApiResponse<T>
+where
+    T: Serialize + Default,
+{
     let res = ApiResponse {
         result: data,
         ..Default::default()
@@ -13,7 +16,10 @@ pub fn box_type_rest_response<T>(data: T) -> ApiResponse<T> where T: Serialize +
 }
 
 // https://stackoverflow.com/questions/76987201/distinct-uses-of-impl-trait-result-in-different-opaque-types-in-rust
-pub fn box_actix_rest_response<T>(data: T) -> HttpResponse where T: Serialize + Default{
+pub fn box_actix_rest_response<T>(data: T) -> HttpResponse
+where
+    T: Serialize + Default,
+{
     let res = ApiResponse {
         result: data,
         ..Default::default()
@@ -21,24 +27,30 @@ pub fn box_actix_rest_response<T>(data: T) -> HttpResponse where T: Serialize + 
     HttpResponse::Ok().json(res)
 }
 
-pub fn box_error_actix_rest_response <T>(data: T, result_code: String, msg: String) -> HttpResponse where T: Serialize + Default {
+pub fn box_error_actix_rest_response<T>(data: T, result_code: String, msg: String) -> HttpResponse
+where
+    T: Serialize + Default,
+{
     let res = ApiResponse {
         result: data,
         statusCode: "200".to_string(),
         resultCode: result_code,
-        msg
+        msg,
     };
     HttpResponse::Ok().json(res)
 }
 
 /// sometimes we need to keep the response type for docs purpose
 /// https://github.com/GREsau/okapi/issues/102
-pub fn box_error_type_rest_response <T>(data: T, result_code: String, msg: String) -> ApiResponse<T> where T: Serialize + Default {
+pub fn box_error_type_rest_response<T>(data: T, result_code: String, msg: String) -> ApiResponse<T>
+where
+    T: Serialize + Default,
+{
     let res = ApiResponse {
         result: data,
         statusCode: "200".to_string(),
         resultCode: result_code,
-        msg
+        msg,
     };
     return res;
 }
