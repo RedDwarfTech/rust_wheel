5107a85d5209de3ae41df3f484f89e25e758f664
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-15 20:46:43 +0800

    refactor: twaek the time unit

--- src/texhub/project.rs
+++ src/texhub/project.rs
@@ -2,7 +2,8 @@ use chrono::{NaiveDateTime, Datelike};
 use crate::common::util::rd_file_util::join_paths;
 
 pub fn get_proj_path(base_dir: &String, created_time: i64) -> String {
-    let datetime = NaiveDateTime::from_timestamp_opt(created_time, 0);
+    let created_time_in_seconds = created_time / 1000;
+    let datetime = NaiveDateTime::from_timestamp_opt(created_time_in_seconds, 0);
     let year = datetime.unwrap().year();
     let month = datetime.unwrap().month();
     return join_paths(&[base_dir.as_str(), year.to_string().as_str(), month.to_string().as_str()]);
