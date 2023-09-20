0a32b7c6333c8a018557ca7d83661a3f87df785e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-21 01:01:16 +0800

    feat: get jwt token from Authorization

--- src/model/user/jwt_auth.rs
+++ src/model/user/jwt_auth.rs
@@ -1,12 +1,12 @@
-use core::fmt;
-use std::future::{ready, Ready};
 use super::login_user_info::LoginUserInfo;
 use actix_web::error::ErrorUnauthorized;
 use actix_web::{dev::Payload, Error as ActixWebError};
 use actix_web::{FromRequest, HttpRequest};
+use core::fmt;
 use reqwest::header::{HeaderValue, ToStrError};
 use serde::Serialize;
 use serde_json::from_str;
+use std::future::{ready, Ready};
 
 #[derive(Debug, Serialize)]
 struct ErrorResponse {
@@ -25,41 +25,50 @@ fn get_header_value(header_value: &HeaderValue) -> Result<&str, ToStrError> {
     return value_str;
 }
 
+fn get_auth_header(req: &HttpRequest) -> &str {
+    if let Some(auth_header) = req.headers().get("Authorization") {
+        if let Ok(header_value) = auth_header.to_str() {
+            if header_value.starts_with("Bearer ") {
+                let token = header_value.trim_start_matches("Bearer ");
+                return token;
+            }
+        }
+    }
+    return "";
+}
+
 impl FromRequest for LoginUserInfo {
     type Error = ActixWebError;
     type Future = Ready<Result<Self, Self::Error>>;
     fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
-        let token = req.headers().get("x-access-token");
+        let token = get_auth_header(req);
         let x_request_id = req.headers().get("x-request-id");
-        match token {
-            Some(value) => {
-                let t = get_header_value(value).unwrap();
-                let parts: Vec<&str> = t.split(".").collect();
-                let payload_base64 = parts[1];
-                let payload_str = base64::decode(payload_base64).unwrap();
-                let payload_json = from_str::<serde_json::Value>(&String::from_utf8(payload_str).unwrap()).unwrap();
-                let payload_claims = payload_json.as_object().unwrap();
-                let user_id = payload_claims.get("userId");
-                let app_id = payload_claims.get("appId");
-                let device_id = payload_claims.get("deviceId");
-                let vip_expire_time = payload_claims.get("et");
-                let login_user_info = LoginUserInfo {
-                    token: t.to_string(),
-                    userId: user_id.unwrap().as_i64().unwrap(),
-                    appId: app_id.unwrap().to_string(),
-                    xRequestId: get_header_value(x_request_id.unwrap()).unwrap().to_string(),
-                    deviceId: device_id.unwrap().to_string(),
-                    vipExpireTime: vip_expire_time.unwrap().as_i64().unwrap_or_default()
-                };
-                ready(Ok(login_user_info))
-            }
-            None => {
-                let json_error = ErrorResponse {
-                    status: "fail".to_string(),
-                    message: "the user belonging to this token no logger exists".to_string(),
-                };
-                ready(Err(ErrorUnauthorized(json_error)))
-            }
+        if !token.is_empty() {
+            let parts: Vec<&str> = token.split(".").collect();
+            let payload_base64 = parts[1];
+            let payload_str = base64::decode(payload_base64).unwrap();
+            let payload_json =
+                from_str::<serde_json::Value>(&String::from_utf8(payload_str).unwrap()).unwrap();
+            let payload_claims = payload_json.as_object().unwrap();
+            let user_id = payload_claims.get("userId");
+            let app_id = payload_claims.get("appId");
+            let device_id = payload_claims.get("deviceId");
+            let vip_expire_time = payload_claims.get("et");
+            let login_user_info = LoginUserInfo {
+                token: token.to_string(),
+                userId: user_id.unwrap().as_i64().unwrap(),
+                appId: app_id.unwrap().to_string(),
+                xRequestId: get_header_value(x_request_id.unwrap()).unwrap().to_string(),
+                deviceId: device_id.unwrap().to_string(),
+                vipExpireTime: vip_expire_time.unwrap().as_i64().unwrap_or_default(),
+            };
+            ready(Ok(login_user_info))
+        } else {
+            let json_error = ErrorResponse {
+                status: "fail".to_string(),
+                message: "the user belonging to this token no logger exists".to_string(),
+            };
+            ready(Err(ErrorUnauthorized(json_error)))
         }
     }
 }
