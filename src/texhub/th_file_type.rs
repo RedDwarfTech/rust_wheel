8c53fbd907768014a6562829c99dfdb68349c1bf
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-21 12:26:12 +0800

    feat: add enum to i32

--- src/texhub/th_file_type.rs
+++ src/texhub/th_file_type.rs
@@ -3,3 +3,12 @@ pub enum ThFileType {
     Folder = 0,
     Tex = 1,
 }
+
+impl From<ThFileType> for i32 {
+    fn from(file_type: ThFileType) -> Self {
+        match file_type {
+            ThFileType::Folder => 0,
+            ThFileType::Tex => 1,
+        }
+    }
+}
