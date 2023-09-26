47deaba070ec061a38a8c0f909f996fb3dd20a09
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-26 17:47:40 +0800

    feat: add copy for reddwarf user info

--- src/model/user/rd_user_info.rs
+++ src/model/user/rd_user_info.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct RdUserInfo {
     pub id: String, // https://stackoverflow.com/questions/77032887/is-it-possible-try-to-convert-the-data-type-when-using-rust-serde-json
     pub nickname: String,
