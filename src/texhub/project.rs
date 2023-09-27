d09d3f668d297e6a404f1d804f10fcf05a47e6a5
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-27 23:24:34 +0800

    refactor: tweak get project relative path

--- src/texhub/project.rs
+++ src/texhub/project.rs
@@ -9,10 +9,10 @@ pub fn get_proj_path(base_dir: &String, created_time: i64) -> String {
     return join_paths(&[base_dir.as_str(), year.to_string().as_str(), month.to_string().as_str()]);
 }
 
-pub fn get_proj_relative_path(proj_id: &String, created_time: i64, version: &String) -> String {
+pub fn get_proj_relative_path(proj_id: &String, created_time: i64) -> String {
     let created_time_in_seconds = created_time / 1000;
     let datetime = NaiveDateTime::from_timestamp_opt(created_time_in_seconds, 0);
     let year = datetime.unwrap().year();
     let month = datetime.unwrap().month();
-    return join_paths(&[year.to_string().as_str(), month.to_string().as_str(), proj_id, version]);
+    return join_paths(&[year.to_string().as_str(), month.to_string().as_str(), proj_id]);
 }
