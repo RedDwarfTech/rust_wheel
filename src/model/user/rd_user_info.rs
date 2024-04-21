23c40b371e14be802dbabb8005ef0a82d74bb570
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-04-21 14:28:07 +0800

    feat: add default for reddwarf user

--- src/model/user/rd_user_info.rs
+++ src/model/user/rd_user_info.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 pub struct RdUserInfo {
     pub id: i64, // https://stackoverflow.com/questions/77032887/is-it-possible-try-to-convert-the-data-type-when-using-rust-serde-json
     pub nickname: String,
