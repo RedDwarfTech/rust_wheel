aca0c1493ae0307813f31e07bb6883bf9213afa2
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-04-21 13:07:05 +0800

    feat: add field for rd user info

--- src/model/user/rd_user_info.rs
+++ src/model/user/rd_user_info.rs
@@ -5,5 +5,11 @@ pub struct RdUserInfo {
     pub id: i64, // https://stackoverflow.com/questions/77032887/is-it-possible-try-to-convert-the-data-type-when-using-rust-serde-json
     pub nickname: String,
     pub device_id: String,
-    pub app_id: String
+    pub app_id: String,
+    #[serde(rename = "avatarUrl")]
+    pub avatar_url: String,
+    #[serde(rename = "autoRenewProductExpireTimeMs")]
+    pub auto_renew_product_expire_time_ms: i64,
+    #[serde(rename = "appName")]
+    pub app_name: String,
 }
