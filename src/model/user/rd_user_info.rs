c468e55a797f11d309b477e24bd1bd9c14056df3
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-04-21 22:36:36 +0800

    refactor: tweak the rd user info

--- src/model/user/rd_user_info.rs
+++ src/model/user/rd_user_info.rs
@@ -2,9 +2,12 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Clone, Default)]
 pub struct RdUserInfo {
+    #[serde(rename = "userId")]
     pub id: i64, // https://stackoverflow.com/questions/77032887/is-it-possible-try-to-convert-the-data-type-when-using-rust-serde-json
     pub nickname: String,
+    #[serde(rename = "deviceId")]
     pub device_id: String,
+    #[serde(rename = "appId")]
     pub app_id: String,
     #[serde(rename = "avatarUrl")]
     pub avatar_url: String,
