54effde8966efe542b1560a7c85c41f665ddcbf8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-05-04 16:22:50 +0800

    refactor: tweak the legacy verify

--- src/alipay/api/internal/util/alipay_signature.rs
+++ src/alipay/api/internal/util/alipay_signature.rs
@@ -15,12 +15,20 @@ pub fn rsa_check_v1(params: &mut HashMap<String, String>, public_key: String) ->
         error!("content is null, params: {:?}", params);
         return Err(SignatureError::SignContentNull);
     }
+    let tmp_content = content.unwrap_or_default();
+    let origin_content_str = urlencoding::decode(&tmp_content);
+    let origin_content = origin_content_str.unwrap_or_default().into_owned();
+
+    let tmp_sign = sign.unwrap();
+    let origin_sign_str = urlencoding::decode(&tmp_sign);
+    let origin_sign = origin_sign_str.unwrap_or_default().into_owned();
+    let base64_decode = base64::decode(&origin_sign).unwrap_or_default();
     // https://docs.rs/ring/latest/ring/signature/index.html
     let verify_public_key = signature::UnparsedPublicKey::new(
         &signature::RSA_PKCS1_2048_8192_SHA256,
         public_key.as_bytes(),
     );
-    verify_public_key.verify(content.clone().unwrap().as_bytes(), &sign.unwrap().as_bytes()).map_err(|_| SignatureError::BadSignature) 
+    verify_public_key.verify(origin_content.as_bytes(), &base64_decode).map_err(|_| SignatureError::BadSignature) 
 }
 
 pub fn get_sign_check_content_v1(params: &mut HashMap<String, String>) -> Option<String> {
