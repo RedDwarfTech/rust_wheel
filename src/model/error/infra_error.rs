cc730a7ac82fb054621e31802b54bab5e726ccde
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-06-02 16:31:45 +0800

    [feat] add infra error define

---
+++ src/model/error/infra_error.rs
@@ -0,0 +1,26 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum InfraError {
+    #[error("新旧密码不能相同")]
+    NewOldPwdDuplicate,
+}
+impl InfraError {
+    pub fn error_code(&self) -> u32 {
+        match self {
+            InfraError::NewOldPwdDuplicate => 0030010008,
+        }
+    }
+
+    pub fn error_message(&self) -> &str {
+        match self {
+            InfraError::NewOldPwdDuplicate => "新旧密码不能相同"            
+        }
+    }
+
+    pub fn error_code_en(&self) -> &str {
+        match self {
+            InfraError::NewOldPwdDuplicate => "NEW_OLD_PWD_DUPLICATED"
+        }
+    }
+}
--- src/model/error/mod.rs
+++ src/model/error/mod.rs
@@ -1,3 +1,4 @@
 pub mod custom_error;
 pub mod user;
 pub mod my_custom_error;
+pub mod infra_error;
