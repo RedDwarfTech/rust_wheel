4f45f78f629fe279dc95e5de2f12facae906277e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-06-08 21:22:47 +0800

    refactor: add pwd complex check error

--- src/model/error/infra_error.rs
+++ src/model/error/infra_error.rs
@@ -4,23 +4,28 @@ use thiserror::Error;
 pub enum InfraError {
     #[error("新旧密码不能相同")]
     NewOldPwdDuplicate,
+    #[error("密码不够安全,密码必须包含大写、小写、数字和特殊字符，且长度是8-32位")]
+    PwdNitMatchComplexGuide,
 }
 impl InfraError {
     pub fn error_code(&self) -> &str {
         match self {
             InfraError::NewOldPwdDuplicate => "0030010008",
+            InfraError::PwdNitMatchComplexGuide => "0030010006"
         }
     }
 
     pub fn error_message(&self) -> &str {
         match self {
-            InfraError::NewOldPwdDuplicate => "新旧密码不能相同"            
+            InfraError::NewOldPwdDuplicate => "新旧密码不能相同",
+            InfraError::PwdNitMatchComplexGuide => "密码不够安全,密码必须包含大写、小写、数字和特殊字符，且长度是8-32"            
         }
     }
 
     pub fn error_code_en(&self) -> &str {
         match self {
-            InfraError::NewOldPwdDuplicate => "NEW_OLD_PWD_DUPLICATED"
+            InfraError::NewOldPwdDuplicate => "NEW_OLD_PWD_DUPLICATED",
+            InfraError::PwdNitMatchComplexGuide => "PWD_NOT_MATCH_COMPLAEX_GUIDE"
         }
     }
 }
