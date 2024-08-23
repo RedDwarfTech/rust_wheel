6b05905e65eec3aab8fbfa41baba8e84ac6dbc85
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-08-23 22:23:14 +0800

    feat: tweak the get string sha

--- src/common/util/security_util.rs
+++ src/common/util/security_util.rs
@@ -1,5 +1,5 @@
-use ring::digest;
 use data_encoding::HEXLOWER;
+use ring::digest::{self, Algorithm};
 
 pub fn get_sha(password: String, salt: &str) -> String {
     let calc = salt.to_owned() + &password.to_string();
@@ -8,9 +8,8 @@ pub fn get_sha(password: String, salt: &str) -> String {
     return calc_digest_str;
 }
 
-pub fn get_str_sha(hash_str: String) -> String {
-    let actual = digest::digest(&digest::SHA512, hash_str.as_ref());
+pub fn get_str_sha(hash_str: String, algorithm: &'static Algorithm) -> String {
+    let actual = digest::digest(algorithm, hash_str.as_ref());
     let calc_digest_str = HEXLOWER.encode(actual.as_ref());
     return calc_digest_str;
 }
-
