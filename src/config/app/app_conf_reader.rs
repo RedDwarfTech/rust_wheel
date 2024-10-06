609d2b2f9a970d707c82a7d407dd72399b5f1c5c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-10-06 14:22:34 +0800

    refactor: add i64 config get

--- src/config/app/app_conf_reader.rs
+++ src/config/app/app_conf_reader.rs
@@ -1,10 +1,10 @@
 use config::{Config, File};
 use log::error;
 
-pub fn get_app_config(key: &str) -> String{
+pub fn get_app_config(key: &str) -> String {
     let mut settings = Config::default();
     let merge_result = settings.merge(File::with_name("settings"));
-    if let Err(err) = merge_result { 
+    if let Err(err) = merge_result {
         error!("merge config error: {}, key:{}", err, key);
         return "".to_string();
     }
@@ -14,4 +14,23 @@ pub fn get_app_config(key: &str) -> String{
         return "".to_string();
     }
     return read_result.unwrap();
+}
+
+pub fn get_i64_app_config(key: &str) -> Option<i64> {
+    let mut settings = Config::default();
+    let merge_result = settings.merge(File::with_name("settings"));
+    if let Err(err) = merge_result {
+        error!("merge config error: {}, key:{}", err, key);
+        return None;
+    }
+    let read_result = settings.get::<String>(key);
+    if let Err(e) = read_result {
+        error!("read config error: {}, key: {}", e, key);
+        return None;
+    }
+    let parse_result = read_result.unwrap().parse::<i64>();
+    if let Err(e) = parse_result.as_ref() {
+        error!("parse config failed,{}", e);
+    }
+    return Some(parse_result.unwrap());
 }
