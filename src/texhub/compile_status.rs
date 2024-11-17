a66abd89c22dc03c0ce54c5d60bc0f323907614d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-11-17 14:52:07 +0800

    feat: add tex file compile status

--- src/texhub/compile_status.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug, PartialEq, PartialOrd, Eq)]
-pub enum CompileStatus {
-    Queued = 0,
-    Compiling = 1,
-    Complete = 2,
-}
--- src/texhub/mod.rs
+++ src/texhub/mod.rs
@@ -1,4 +1,4 @@
-pub mod compile_status;
+pub mod tex_file_compile_status;
 pub mod project;
 pub mod th_file_type;
 pub mod proj;
